cba8ee0c1adee82aa2fa263ac9a6dd215d248174
JhovanniDRiveraG <riverajhovanni1@gmail.com>
2025-04-18 13:00:55 -0500

    archivos imprimir e ingreso

---
+++ imprimir.py
@@ -0,0 +1,20 @@
+a = "Carlos"
+b = 20
+print("hola", a, "tienes ", b)
+
+c = "Hola " + str(a) + " tienes " + str(b)
+print(c)
+
+d = f"Hola  {a} + tienes  {b}"#interpolacion como $ en C#
+print(d)
+
+
+
+print(c)
+
+print("Carlos", "Jose", "Juan")
+print("Carlos", "Jose", "Juan", sep="* ",end="# ")
+print("A", "B", "C")
+
+
+print(c)
---
+++ programa.py
@@ -0,0 +1,16 @@
+a = 2**3
+
+if a == 8:
+    print(a)
+else:
+    print("no")
+
+
+b = "hola"+" "+"amigo"
+print(b)
+
+c = "Estudia" * 5
+print(c)
+
+
+# comentario 
